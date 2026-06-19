d7befc3b557c61605c2cfc82038d10fbe4a6deea
neilviradia <nviradia30@gmail.com>
2026-06-19 19:08:12 -0400

    >moved bulk caching to main teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuzzTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuzzTeleOp.java
@@ -2,13 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.List;
+
 @TeleOp(name = "TeleOp", group = "Buzz")
 @Disabled
 public class BuzzTeleOp extends LinearOpMode {
+    List<LynxModule> allHubs;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -17,9 +21,17 @@ public class BuzzTeleOp extends LinearOpMode {
 
         Superstructure ss = new Superstructure(gamepad1, gamepad2, hardwareMap, telemetry);
 
+        allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule module : allHubs) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
         waitForStart();
 
         while (opModeIsActive()) {
+            for (LynxModule module : allHubs) {
+                module.clearBulkCache();
+            }
 
             ss.Periodic();
 
