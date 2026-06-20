a6ea850ae42dd3dae11aa409eb47f97effff99b3
neilviradia <nviradia30@gmail.com>
2026-06-20 09:22:56 -0400

    >automatic outtake when intake stalls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -8,17 +8,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 public class IntakeSubsystem extends Constants{
-    final DcMotor intake;
+    final DcMotorEx intake;
     final Telemetry telemetry;
     ElapsedTime outtakeTimer = new ElapsedTime();
     Gamepad opCon;
     public boolean initDone = false;
     public IntakeSubsystem(Gamepad opCon, HardwareMap hardwareMap, Telemetry telemetry){
-        intake = hardwareMap.get(DcMotor.class,"intake");
+        intake = hardwareMap.get(DcMotorEx.class,"intake");
 
-        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake.setDirection(DcMotorEx.Direction.REVERSE);
 
         opCon.setTriggerThreshold(triggerThresh);
 
@@ -32,14 +33,13 @@ public class IntakeSubsystem extends Constants{
     public void collect(){
         boolean intakeOn = opCon.right_trigger_pressed;
 
-        if (intakeOn) {
-            intake.setPower(1);
-            if (intake.getPower() < 0.8){
-                outtakeTimer.reset();
-                while (outtakeTimer.milliseconds() < 1500) {
-                    intake.setPower(-1);
-                }
+        if (intakeOn && intake.getCurrent(CurrentUnit.AMPS) > 9) {
+            outtakeTimer.reset();
+            if (outtakeTimer.milliseconds() < 250) {
+                intake.setPower(-1);
             }
+        } else if (intakeOn) {
+            intake.setPower(1);
         } else {
             intake.setPower(0);
         }
